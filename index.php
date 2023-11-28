fcebc97ca67a33da685c76d9ac499e9d08926b7d
Jehan Ratnakumar <90109380+jehan93@users.noreply.github.com>
2023-11-28 23:46:42 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+if (!empty($_SERVER['HTTPS']) && ('on' == $_SERVER['HTTPS'])) {
+		$uri = 'https://';
+	} else {
+		$uri = 'http://';
+	}
+	$uri .= $_SERVER['HTTP_HOST'];
+	header('Location: '.$uri.'/CMSL_HRM_2/Apps/view/dashboard.php');
+	exit;
+?>
